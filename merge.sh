ada56bc17e2dde1ec6b5cdb874dcd0a738fb6260
nosillytrying_tang <nosillytrying_tang@163.com>
2020-12-24 20:08:10 +0800

    reason

--- merge.sh
+++ merge.sh
@@ -5,7 +5,10 @@
 
 if [[ $1 && $2 && $1!=$2 ]];
     then
-        echo '这里可以直接进行合并了'
+        git add .
+        read -p '请输入提交信息' reason
+        git commit -m reason
+        echo "本地commit完成"
     else 
     read -p '请输入要合并的目标分支'  branch1;
     read -p '请输入要合并的原分支'  branch2;
@@ -14,7 +17,7 @@ if [[ $1 && $2 && $1!=$2 ]];
         if [[ $branch1 = $branch2 ]];
         then echo '请不要输入两个相同的目标分支';
         else
-          echo "################## 开始 git fetch 开始 ########################"
+          echo "################## 开始 git fetch 开始 @@@@@@########################"
           git add .
           read -p '请输入提交信息' reason
           git commit -m reason;
